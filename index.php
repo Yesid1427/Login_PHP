1ed8b2b5e68c7f373b1a8a358f82ff9d4b6aa281
YESID\Administrador <yesid1427@hotmail.com>
2018-03-12 13:19:04 -0500

    Login_PHP

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+?>
+<html>
+	<head>
+		<title>.: RLPHP :.</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+	<div class="container">
+	<div class="row">
+	<div class="col-md-12">
+			<h2>REGISTRO Y LOGIN BASICO</h2>
+			<p class="lead">Sistema de Registro y Login Sencillo con PHP y MySQL</p>
+			<p>Un sistema de registro y login sencillo.</p>
+			<p>Instrucciones:</p>
+			<ol>
+				<li>Regístrate en la opción de registro.</li>
+				<li>Inicie sesion en la opción de login.</li>
+				<li>Para finalizar la sesión, click en la opción salir .</li>
+			</ol>
+			<br>
+			<ul type="none">
+			<li><i class="glyphicon glyphicon-ok"></i> Fácil de instalar y modificar</li>
+			<li><i class="glyphicon glyphicon-ok"></i> Ideal para tu próximo proyecto web</li>
+			</ul>
+
+	</div>
+	</div>
+	</div>
+	</body>
+</html>
---
+++ php/conexion.php
@@ -0,0 +1,8 @@
+<?php
+$host="localhost";
+$user="root";
+$password="Intel1427";
+$db="myapp";
+$con = new mysqli($host,$user,$password,$db);
+
+?>
